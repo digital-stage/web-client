db220d2523ddb1e5248aaecc35789368354997c2
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-05-12 23:51:40 +0200

    Ios fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,9 @@ class DigitalStageDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Digital Stage</title>
           <link href="/static/fonts/fonts.css" rel="stylesheet"/>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width, shrink-to-fit=no"/>
         </Head>
         <body>
         <Main/>
