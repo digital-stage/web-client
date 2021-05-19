4c1f435e0b2d3fe9bc78e4bb13dbaf99e401c1bf
delude88 <tobias.hegemann@googlemail.com>
2021-05-19 21:51:42 +0200

    Linted files

--- components/mixer/VolumeSlider/utils.ts
+++ components/mixer/VolumeSlider/utils.ts
@@ -1,5 +1,5 @@
 export function getBaseLog(x: number, y: number): number {
-    return Math.log(y) / Math.log(x)
+  return Math.log(y) / Math.log(x);
 }
 
 /** *
@@ -7,22 +7,22 @@ export function getBaseLog(x: number, y: number): number {
  * @param value, float value between 0 and 1
  */
 export const convertRangeToDbMeasure = (value: number): number => {
-    if (value > 0) {
-        return 20 * Math.log10(value)
-    }
-    return Number.NEGATIVE_INFINITY
-}
+  if (value > 0) {
+    return 20 * Math.log10(value);
+  }
+  return Number.NEGATIVE_INFINITY;
+};
 
 export const convertDbMeasureToRange = (value: number): number => {
-    return value ** 10 / 20
-}
+  return value ** 10 / 20;
+};
 
 export const formatDbMeasure = (value: number, unit?: boolean): string => {
-    if (value > Number.NEGATIVE_INFINITY) {
-        let str: string = (Math.round(value * 10) / 10).toString()
-        if (value > 0) str = `+${str}`
-        if (unit) str += 'db'
-        return str
-    }
-    return '-∞'
-}
+  if (value > Number.NEGATIVE_INFINITY) {
+    let str: string = (Math.round(value * 10) / 10).toString();
+    if (value > 0) str = `+${str}`;
+    if (unit) str += 'db';
+    return str;
+  }
+  return '-∞';
+};
