dc88ab008b0124c06b705c55a525bb7d066f1b69
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-06-18 18:58:17 +0200

    Current

--- components/room/RoomView.tsx
+++ components/room/RoomView.tsx
@@ -1,15 +1,33 @@
 import { fabric } from 'fabric'
 import { useEffect, useRef, useState } from 'react'
-import { Canvas } from 'fabric/fabric-impl'
+import { Canvas, Rect } from 'fabric/fabric-impl'
+import { Group } from '@digitalstage/api-types'
 import styles from './RoomView.module.css'
 import { useStageSelector } from '../../api-client-react'
+
+const GroupRenderer = ({ groupId, canvas }: { groupId: string; canvas?: Canvas }) => {
+    const group = useStageSelector<Group>((state) => state.groups.byId[groupId])
+    const [rect] = useState<Rect>(
+        new fabric.Rect({ top: 100, left: 100, width: 100, height: 100, fill: '#f55', angle: 45 })
+    )
+
+    useEffect(() => {
+        if (canvas && rect) canvas.add(rect)
+    }, [canvas, rect])
+
+    useEffect(() => {
+        if (group && rect) {
+            // rect.top = group.x
+        }
+    }, [group, rect])
+
+    return null
+}
 
 const StageView = ({ stageId }: { stageId: string }) => {
     const [canvas, setCanvas] = useState<Canvas>()
     const canvasRef = useRef<HTMLCanvasElement>()
-    const groups = useStageSelector((state) =>
-        state.groups.byStage[stageId].map((id) => state.groups.byId[id])
-    )
+    const groupIds = useStageSelector((state) => state.groups.byStage[stageId])
 
     useEffect(() => {
         if (canvasRef.current) {
@@ -17,7 +35,14 @@ const StageView = ({ stageId }: { stageId: string }) => {
         }
     }, [canvasRef])
 
-    return <canvas className={styles.canvas} ref={canvasRef} />
+    return (
+        <>
+            <canvas className={styles.canvas} ref={canvasRef} />
+            {groupIds.map((id) => (
+                <GroupRenderer groupId={id} canvas={canvas} />
+            ))}
+        </>
+    )
 }
 
 const RoomView = () => {
